cbaf9a33553f4bf0d30aa3c138bc6125fe89e5b6
PlasticDept <plastic.npi.lc3a@outlook.com>
2025-05-10 15:12:39 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,14 @@
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAftyswK9lUac7Glft4jpFhzh5We__Fnn8",
+    authDomain: "outbound-job.firebaseapp.com",
+    databaseURL: "https://outbound-job-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "outbound-job",
+    storageBucket: "outbound-job.firebasestorage.app",
+    messagingSenderId: "474062052494",
+    appId: "1:474062052494:web:f0dc128cc1693ddea73ddf"
+  };
 
+  // Inisialisasi Firebase
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();

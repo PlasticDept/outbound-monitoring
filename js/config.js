8df46cd6aec7ab1c540f223b232f5ca11dd70b2b
PlasticDept <plastic.npi.lc3a@outlook.com>
2025-05-11 08:04:20 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,16 @@
-// Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAftyswK9lUac7Glft4jpFhzh5We__Fnn8",
-    authDomain: "outbound-job.firebaseapp.com",
-    databaseURL: "https://outbound-job-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "outbound-job",
-    storageBucket: "outbound-job.firebasestorage.app",
-    messagingSenderId: "474062052494",
-    appId: "1:474062052494:web:f0dc128cc1693ddea73ddf"
-  };
+// config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-  // Inisialisasi Firebase
-  firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
+const firebaseConfig = {
+  apiKey: "AIzaSyAftyswK9lUac7Glft4jpFhzh5We__Fnn8",
+  authDomain: "outbound-job.firebaseapp.com",
+  databaseURL: "https://outbound-job-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "outbound-job",
+  storageBucket: "outbound-job.appspot.com",
+  messagingSenderId: "474062052494",
+  appId: "1:474062052494:web:f0dc128cc1693ddea73ddf"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);

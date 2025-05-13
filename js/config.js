0d77d4045940753d2e3f10635dc5f2e287406c0a
PlasticDept <plastic.npi.lc3a@outlook.com>
2025-05-13 19:03:24 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAftyswK9lUac7Glft4jpFhzh5We__Fnn8",
-  authDomain: "outbound-job.firebaseapp.com",
-  databaseURL: "https://outbound-job-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "outbound-job",
-  storageBucket: "outbound-job.appspot.com",
-  messagingSenderId: "474062052494",
-  appId: "1:474062052494:web:f0dc128cc1693ddea73ddf"
+  apiKey: "AIzaSyAfIYig9-sv3RfazwAW6X937_5HJfgnYt4",
+  authDomain: "outobund.firebaseapp.com",
+  databaseURL: "https://outobund-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "outobund",
+  storageBucket: "outobund.firebasestorage.app",
+  messagingSenderId: "84643346476",
+  appId: "1:84643346476:web:beb19c5ea0884fcb083989"
 };
 
 const app = initializeApp(firebaseConfig);
